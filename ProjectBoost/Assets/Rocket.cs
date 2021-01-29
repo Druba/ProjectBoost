5c941e4e85a6deafb860c43aae0746b3d5f6c54f
Druba <andrasmorvay@gmail.com>
2021-01-28 23:12:02 -0800

    Fixing Rotation and Thrust

--- ProjectBoost/Assets/Rocket.cs
+++ ProjectBoost/Assets/Rocket.cs
@@ -7,6 +7,8 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource thrust;
+    [SerializeField] float rcsThrust = 115f;
+    [SerializeField] float mainThrust = 1000f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +21,17 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space))
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!thrust.isPlaying) //so it doesn't layer
             {
                 thrust.Play();
@@ -38,15 +43,27 @@ public class Rocket : MonoBehaviour
         {
             thrust.mute = true;
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
             print("Rotating left.");
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
             print("Rotating right.");
         }
+
+        rigidBody.freezeRotation = false; //resume physics control of rotation
+
     }
+
+
 }
